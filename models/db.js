c80e35f7110b06bdebdf103dc2e01092f6f6fab4
Pradeepbkv <4kochuse@gmail.com>
2025-07-08 21:56:26 +0530

    Add files via upload

--- models/db.js
+++ models/db.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false, // Required for Render-hosted PostgreSQL
   }
 });
 
 module.exports = pool;
+
 
